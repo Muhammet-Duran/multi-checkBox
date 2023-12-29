fc7760dfc3d179a5ea684e70a3dd525b190e1133
Muhammet-Duran <mhmmtduran01@gmail.com>
2023-12-30 00:12:14 +0300

    General settings added to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div className='App'>react projesi</div>;
+  return <div className='text-[50px] text-red-500'>react projesi</div>;
 };
 
 export default App;
